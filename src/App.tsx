8767d441781488fc38eebe3e445320e76109eb46
chocoanish <chocoboyanish566@gmail.com>
2025-04-05 00:02:12 +0530

    test: husky hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './styles.css';
 import SEO from './SEO';
 
-const App = () => (
+
+const App = () => {
+  const data = 0;
+  return(
   <div className="app-container">
     <SEO title="My App" description="A React + TypeScript + Webpack template." />
 
     <h1>Welcome to My App</h1>
   </div>
-)
+  )
+}
 
 export default App
